eec980f43057192ee5ec637c7c91639b9921cb9f
Artyom Surgutanov <surgart@ya.ru>
2023-07-05 10:00:03 +0300

    Fix: Добавил использование асинхронности совместно с многопоточностью

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,14 @@
 use std::collections::HashMap;
-use std::num::NonZeroUsize;
-use std::thread;
 use std::fs::File;
 use std::io::{self, prelude::*, SeekFrom, Seek};
-use std::path::Path;
-use std::sync::{Arc, Mutex};
+use std::path::{Path, PathBuf};
 
+use itertools::Itertools;
 use walkdir::WalkDir;
-use threadpool::ThreadPool;
 
 
-/// Walk through `directory` and it's subdirectories and return iterator of files
-fn walkfiles<P>(directory: P) -> impl Iterator<Item = Result<walkdir::DirEntry, walkdir::Error>>
+/// Walks through `directory` and it's subdirectories and returns iterator of files
+fn walkfiles<P>(directory: P) -> impl Iterator<Item = PathBuf>
 where P: AsRef<Path>, {
     WalkDir::new(directory)
         .into_iter()
@@ -24,11 +21,14 @@ where P: AsRef<Path>, {
                 },
             }
         })
+        .map(|entry| {
+            entry.expect("Expected filepath").into_path()
+        })
 }
 
-/// Counts `substring` in a chunk, returns `count` and `position` of the last match
+/// Counts `substring` matches in a chunk, returns `count` and `position` of the last match
 /// If `count` is zero (no any matches) then `position` equals to zero
-fn count_substring_in_chunk(chunk: &[u8], substring: &str) -> (usize, usize) {
+fn count_matches_in_chunk(chunk: &[u8], substring: &str) -> (usize, usize) {
     let mut counter: usize = 0;
     let substring = substring.as_bytes();
     let substring_length = substring.len();
@@ -51,8 +51,8 @@ fn count_substring_in_chunk(chunk: &[u8], substring: &str) -> (usize, usize) {
     (counter, pos_of_the_last_match)
 }
 
-/// Count `substring` in a file
-fn count_substring_in_file<P>(filepath: P, substring: &str) -> Result<usize, io::Error>
+/// Count `substring` matches in a file
+fn count_matches_in_file<P>(filepath: P, substring: &str) -> Result<usize, io::Error>
 where P: AsRef<Path>, {
     const BUFFER_SIZE: usize = 8192;
     let mut buffer = [0 as u8; BUFFER_SIZE];
@@ -68,7 +68,7 @@ where P: AsRef<Path>, {
         }
 
         let (chunk, _) = buffer.split_at(size);
-        let (count, pos_of_the_last_match) = count_substring_in_chunk(chunk, substring);
+        let (count, pos_of_the_last_match) = count_matches_in_chunk(chunk, substring);
         counter += count;
 
         // Calculate next `seekfrom`
@@ -88,43 +88,65 @@ where P: AsRef<Path>, {
     Ok(counter)
 }
 
-/// Count `substring` in files in a given `directory` and collect results into HashMap
-pub fn count_substring_in_files<P>(directory: P, substring: &str)
-where P: AsRef<Path> + {
-    let storage: HashMap<String, usize> = HashMap::new();
-    let storage_mutex = Arc::new(Mutex::new(storage));
-
-    let n_workers = NonZeroUsize::new(8).expect("Count of workers must be non zero usize");
-    let n_workers = thread::available_parallelism().unwrap_or(n_workers);
-    let pool = ThreadPool::new(n_workers.get());
-
-    for entry in walkfiles(directory) {
-        match entry {
-            Ok(entry) => {
-                let storage = Arc::clone(&storage_mutex);
-                let filepath = String::from(entry.path().to_str().unwrap());
-                let substring = String::from(substring);
-                pool.execute(move || {
-                    let result = count_substring_in_file(&filepath, &substring);
-                    match result {
-                        Ok(count) => {
-                            let mut storage = storage.lock().unwrap();
-                            let default = 0;
-                            let value = storage.get(&filepath).unwrap_or(&default);
-                            let value = value + count;
-                            storage.insert(filepath, value);
-                        },
-                        Err(error) => eprintln!("{}: {}", filepath, error),
-                    };
-                });
+/// Count `substring` in a file asynchronously
+pub async fn async_count_substring_in_file(filepath: String, substring: String) -> Option<usize>
+{
+    let (send, recv) = tokio::sync::oneshot::channel();
+    let filepath = filepath.to_string();
+    let substring = substring.to_string();
+    rayon::spawn(move || {
+        let filepath = filepath.as_str();
+        let substring = substring.as_str();
+        let result = count_matches_in_file(filepath, substring);
+        match result {
+            Ok(count) => {
+                let _ = send.send(count);
             },
-            Err(err) => eprintln!("{err}"),
+            Err(error) => eprintln!("{}: {}", filepath, error),
         };
-    };
-    pool.join();
+    });
 
-    let storage_mutex = Arc::try_unwrap(storage_mutex).unwrap();
-    let storage = storage_mutex.into_inner().unwrap();
+    match recv.await {
+        Ok(count) => Some(count),
+        Err(error) => {
+            eprintln!("Thread message: {error}");
+            None
+        }
+    }
+}
+
+/// Count asynchronously `substring` in files in a given `directory` and print results into stdio
+pub async fn async_count_substring_in_files<P>(directory: P, substring: String)
+where P: AsRef<Path>, {
+    let tasks = walkfiles(directory)
+    .map(|path| {
+        let filepath = path.as_path().to_str().unwrap().to_string();
+        let filepath2 = filepath.clone();
+        let substring = substring.clone();
+        (filepath, async_count_substring_in_file(filepath2, substring))
+    });
+
+    let mut storage: HashMap<String, usize> = HashMap::new();
+    const CHUNK_SIZE: usize = 500;
+    for chunk in tasks.chunks(CHUNK_SIZE).into_iter() {
+        let mut results = Vec::with_capacity(CHUNK_SIZE);
+        let chunk: Vec<_> = chunk.collect();
+
+        for (filepath, future) in chunk {
+            results.push((filepath, tokio::spawn(future)));
+        }
+        for (filepath, future) in results {
+            match future.await {
+                Ok(result) => {
+                    if let Some(count) = result {
+                        storage.insert(filepath, count);
+                    }
+                },
+                Err(error) => eprintln!("{filepath}: {error}"),
+            };
+        }
+    }
+
     let result = serde_json::to_string_pretty(&storage);
     match result {
         Ok(json) => println!("{}", json),
@@ -134,20 +156,20 @@ where P: AsRef<Path> + {
 
 mod test {
     #[test]
-    fn test_count_substring_in_chunk() {
+    fn test_count_matches_in_chunk() {
         let cases = [
             ("ababababab".as_bytes(), "ab", (5, 8)),
             ("ababababab".as_bytes(), "aba", (2, 4)),
         ];
         for case in cases {
             let (chunk, substring, expected) = case;
-            let count = crate::count_substring_in_chunk(&chunk, &substring);
+            let count = crate::count_matches_in_chunk(&chunk, &substring);
             assert_eq!(count, expected);
         }
     }
 
     #[test]
-    fn test_count_substring_in_small_files() {
+    fn test_count_matches_in_small_files() {
         let cases = [
             ("./samples/small/aaaa10e2.txt", "aaaa", 100),
             ("./samples/small/ababa10e6_no_newlines.txt", "aba", 1000_000),
@@ -157,7 +179,7 @@ mod test {
             ("./samples/small/abcde5432_no_newlines.txt", "bcd", 5432),
         ];
         for (filepath, substring, expected) in cases {
-            let result = crate::count_substring_in_file(filepath, substring);
+            let result = crate::count_matches_in_file(filepath, substring);
             let count = match result {
                 Ok(count) => count,
                 Err(error) => {
@@ -171,12 +193,12 @@ mod test {
 
     #[test]
     #[ignore]
-    fn test_count_substring_in_large_files() {
+    fn test_count_matches_in_large_files() {
         let cases = [
             ("./samples/large/ababa10e9_no_newlines.txt", "aba", 1000_000_000),
         ];
         for (filepath, substring, expected) in cases {
-            let result = crate::count_substring_in_file(filepath, substring);
+            let result = crate::count_matches_in_file(filepath, substring);
             let count = match result {
                 Ok(count) => count,
                 Err(error) => {
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use substringcounter::count_substring_in_files;
+use clap::ArgMatches;
+use substringcounter::async_count_substring_in_files;
 use clap::{Command, Arg};
 
 fn parse_config() -> Command {
@@ -13,11 +14,12 @@ fn parse_config() -> Command {
         .help("Substring"))
 }
 
-fn main(){
-    let command = parse_config();
-    let matches = command.get_matches();
+#[tokio::main]
+async fn main(){
+    let command: Command = parse_config();
+    let matches: ArgMatches = command.get_matches();
 
-    let directory = matches.get_one::<String>("directory").expect("Directory argument is required");
-    let substring: &String = matches.get_one::<String>("substring").expect("Substring argument is required");
-    count_substring_in_files(directory, substring.as_str());
+    let directory = matches.get_one::<String>("directory").expect("Directory argument is required").to_string();
+    let substring = matches.get_one::<String>("substring").expect("Substring argument is required").to_string();
+    async_count_substring_in_files(directory, substring).await;
 }
