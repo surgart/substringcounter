14c8f2557c396b36246d29fb14cccd80df642860
Artyom Surgutanov <surgart@ya.ru>
2023-07-05 07:32:42 +0300

    Fix: Неправильно создавал тестовый экземпляр

--- prepare_small_samples.sh
+++ prepare_small_samples.sh
@@ -7,7 +7,7 @@ printf "aaaa\n%.0s" {1..10} > "$path/aaaa10e1.txt"
 printf "aaaa\n%.0s" {1..100} > "$path/aaaa10e2.txt"
 printf "aaaa\n%.0s" {1..1000} > "$path/aaaa10e3.txt"
 
-for i in {1..10}; do cat "$path/aaaa10e2.txt" >> "$path/aaaa10e4.txt"; done;
+for i in {1..100}; do cat "$path/aaaa10e2.txt" >> "$path/aaaa10e4.txt"; done;
 for i in {1..100}; do cat "$path/aaaa10e3.txt" >> "$path/aaaa10e5.txt"; done;
 for i in {1..1000}; do cat "$path/aaaa10e3.txt" >> "$path/aaaa10e6.txt"; done;
 printf "ababa\n%.0s" {1..1000000} > "$path/ababa10e6.txt"
